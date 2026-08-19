0a1e1d7c4f291af514d604118a6ca1a043f4a574
Aditya Raj <152900366+megatron144@users.noreply.github.com>
2026-08-19 16:05:04 +0530

    Add To Zero [A] from Codeforces

---
+++ 2075/A - To Zero/solution.cpp
@@ -0,0 +1,64 @@
+// ********* ADITYA RAJ **********
+// ********* TEMPLATE **********
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define double long double
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vpi vector<pii>
+#define si set<int>
+#define spi set<pii>
+#define umpi unordered_map<int, int>
+#define mpi map<int, int>
+#define mpp map<pii, int>
+#define pq_max priority_queue<int>
+#define pq_min priority_queue<int, vi, greater<int>>
+#define print(v) for (auto x : v) cout << x << " "; cout << endl
+#define yn(x) cout << (x ? "yes" : "no") << endl
+#define all(v) v.begin(), v.end()
+#define take(v) for (auto &x : v) cin >> x
+#define sum(v) accumulate(v.begin(), v.end(), 0)
+const int N = 200005;
+const int MOD = 1000000007;
+//Hash function to use vector as key in maps
+struct Hash {
+    size_t operator()(const vector<int>& v) const {
+        size_t hash = v.size();
+        for (int i : v) {
+            hash ^= (hash << 5) + (hash >> 2) + i;
+        }
+        return hash;
+    }
+};
+int GCD(int a, int b) {
+    return b == 0 ? a : GCD(b, a % b);
+}
+ 
+int power(int x, int y) {
+    return y ? ((y % 2 ? x : 1ll) * power((x * x) % MOD, y / 2) % MOD) % MOD : 1ll;
+}
+ 
+void test_cases() {
+    int n,k; 
+    cin >> n >> k;
+    int ans = 0;
+    if(n % 2 == 0)
+        ans = (n <= k - 1) ? 1 : (n + k - 2) / (k - 1);
+    else
+        ans = (n <= k) ? 1 : 1 + ((n - k) + k - 2) / (k - 1);
+    cout<<ans<<endl;    
+    return;
+}
+ 
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while(t--)test_cases();
+    return 0;
+}
