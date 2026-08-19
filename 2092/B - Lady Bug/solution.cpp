ec1749298c44780fb25083df000fe35243461935
Aditya Raj <152900366+megatron144@users.noreply.github.com>
2026-08-19 16:06:17 +0530

    Add Lady Bug [B] from Codeforces

---
+++ 2092/B - Lady Bug/solution.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vpi vector<pii>
+#define si set<int>
+#define spi set<pii>
+#define umpi unordered_map<int, int>
+#define mpi map<int, int>
+#define mpp map<pii, int>
+#define pq_max priority_queue<int>
+#define pq_min priority_queue<int, vi, greater<int>>
+#define print(v) for (auto x : v) cout << x << " "; cout << endl
+#define yn(x) cout << (x ? "YES" : "NO") << endl
+#define all(v) v.begin(), v.end()
+#define allR(v) v.rbegin(), v.rend()
+#define take(v) for (auto &x : v) cin >> x
+#define sum(v) accumulate(v.begin(), v.end(), 0)
+const int N = 200005;
+const int MOD = 1e9+7;
+const int MAXN = 1e7;
+struct Hash{
+    size_t operator()(const vector<int>& v) const{
+        size_t hash = v.size();
+        for (int i : v){
+            hash ^= (hash << 5) + (hash >> 2) + i;
+        }
+        return hash;
+    }
+};
+int GCD(int a,int b){
+    while(b){
+        a %= b;
+        swap(a,b);
+    }
+    return a;
+}
+int power(int x,int y){
+    int res = 1;
+    while(y > 0){
+        if(y & 1)
+            res = res * x % MOD;
+        x = x * x % MOD;
+        y >>= 1;
+    }
+    return res;
+}
+vector<bool> prime(MAXN+1, true);
+vi primes;
+void sieve(){
+    prime[0] = prime[1] = false;
+    for (int i = 2; i * i <= MAXN; i++){
+        if(prime[i]){
+            for (int j = i * i; j <= MAXN; j += i)
+                prime[j] = false;
+        }
+    }
+    f(i,2,MAXN+1)
+        if(prime[i])
+            primes.pb(i);
+}
+void test_cases(){
+    int n;
+    cin>>n;
+    string a,b;
+    cin>>a>>b;
+    int cnt1=0,cnt2=0;
+    f(i,0,n){
+        if(!(i&1)){
+            if(b[i]=='1')cnt2++;
+            if(a[i]=='1')cnt1++;
+        }
+        else{
+            if(b[i]=='1')cnt1++;
+            if(a[i]=='1')cnt2++;
+        }
+    }
+    int even=(n&1)?n/2+1:n/2,odd=n-even;
+    yn((cnt1<=odd && cnt2<=even));
+    return;
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while(t--)
+        test_cases();
+    return 0;
+}
